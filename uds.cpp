f1a77491315ef4a30a51bc0a896068cca3e95f05
hkrupp <heiko.krupp@rhrk.uni-kl.de>
2016-06-23 15:21:42 +0200

    First implementation of Diagnostic_Session_Control

--- uds.cpp
+++ uds.cpp
@@ -3,6 +3,21 @@
 #include "uds.h"
 #include <mcp_can.h>
 
+void UDS::print_buffer(uint8_t *buffer, uint16_t len)
+{
+  uint16_t i=0;
+
+  Serial.print(F("Buffer: ")); Serial.print(F(" ["));
+  Serial.print(len); Serial.print(F("] "));
+  for(i=0;i<len;i++)
+  {
+    if(buffer[i] < 0x10) Serial.print(F("0"));
+    Serial.print(buffer[i],HEX);
+    Serial.print(F(" "));
+  }
+  Serial.println();
+}
+
 UDS::UDS(IsoTp* isotp)
 {
   _isotp = isotp;
@@ -10,14 +25,24 @@ UDS::UDS(IsoTp* isotp)
 
 uint8_t UDS::Diagnostic_Session_Control(Session_t* session)
 {
-  uint8_t txbuf[]={0x10, 0x00};
+  struct Message_t msg;
+  uint8_t tmp[MAX_DATA];
+  uint32_t timeout=millis();
+  uint8_t retry=UDS_RETRY;
+  uint8_t retval=0;
 
-  txbuf[1]=session->Data[0];
-  struct Message_t TxMsg;
-  TxMsg.rx_id=session->rx_id;
-  TxMsg.tx_id=session->tx_id;
-  TxMsg.Buffer=txbuf;
-  _isotp->send(&TxMsg);
+  memset(tmp,0,MAX_DATA);
+  tmp[0]=UDS_SID_DIAGNOSTIC_CONTROL;
+  memcpy(tmp+1,session->Data,session->len);
+  msg.rx_id=session->rx_id;
+  msg.tx_id=session->tx_id;
+  msg.len=session->len+1;
+  msg.Buffer=tmp;
+  while(retval=(_isotp->send(&msg) && retry)) retry--;
+  if(!retval) _isotp->receive(&msg);
+  if(millis()-timeout >= UDS_TIMEOUT) retval=1;
+
+  return retval;
 }
 
 uint8_t UDS::ECU_Reset(Session_t* session)
--- uds.h
+++ uds.h
@@ -7,6 +7,36 @@
 //#define UDS_DEBUG
 
 #define MAX_DATA 128
+#define UDS_RETRY 3
+#define UDS_TIMEOUT 500
+#define UDS_KEEPALIVE 3000
+
+/* UDS SIDs */
+#define UDS_SID_DIAGNOSTIC_CONTROL        0x10
+#define UDS_SID_ECU_RESET                 0x11
+#define UDS_SID_CLEAR_DTC                 0x14
+#define UDS_SID_READ_DTC                  0x19
+#define UDS_SID_READ_DATA_BY_ID           0x22
+#define UDS_SID_READ_MEM_BY_ADDRESS       0x23
+#define UDS_SID_READ_SCALING_BY_ID        0x24
+#define UDS_SID_SECURITY_ACCESS           0x27
+#define UDS_SID_READ_DATA_BY_ID_PERIODIC  0x2A
+#define UDS_SID_DEFINE_DATA_ID            0x2C
+#define UDS_SID_WRITE_DATA_BY_ID          0x2E
+#define UDS_SID_IO_CONTROL_BY_ID          0x2F
+#define UDS_SID_ROUTINE_CONTROL           0x31
+#define UDS_SID_REQUEST_DOWNLOAD          0x34
+#define UDS_SID_REQUEST_UPLOAD            0x35
+#define UDS_SID_TRANSFER_DATA             0x36
+#define UDS_SID_REQUEST_XFER_EXIT         0x37
+#define UDS_SID_REQUEST_XFER_FILE         0x38
+#define UDS_SID_WRITE_MEM_BY_ADDRESS      0x3D
+#define UDS_SID_TESTER_PRESENT            0x3E
+#define UDS_SID_ACCESS_TIMING             0x83
+#define UDS_SID_SECURED_DATA_TRANS        0x84
+#define UDS_SID_CONTROL_DTC_SETTINGS      0x85
+#define UDS_SID_RESPONSE_ON_EVENT         0x86
+#define UDS_SID_LINK_CONTROL              0x87
 
 struct Session_t
 {
@@ -20,6 +50,7 @@ class UDS
 {
 	public:
 		UDS(IsoTp* isotp);
+		void    print_buffer(uint8_t *buffer, uint16_t len);
 		uint8_t Diagnostic_Session_Control(Session_t* msg);
 		uint8_t ECU_Reset(Session_t* msg);
 		uint8_t Security_Access(Session_t* msg);
